44d714d5f314b4e198eaaf6a3eb593e430e1ed65
lukehalko <halko@udel.edu>
2021-09-09 22:08:51 -0400

    update TODO nand Change Log. Added party mode button.

---
+++ partyMode.js
@@ -0,0 +1,7 @@
+function partyMode(){
+    let r=Math.floor(Math.random()*255);
+    let g=Math.floor(Math.random()*255);
+    let b=Math.floor(Math.random()*255);
+    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+    console.log(`${r}, ${g}, ${b}`)
+}   
